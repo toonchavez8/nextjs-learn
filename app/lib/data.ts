b15583ed3bd37e86aee6872368a6200a08c333be
Miguel Chavez <toonchavez8@gmail.com>
2024-02-25 22:22:18 -0600

    Feat: Add Aria Labels | Ch 14

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -168,6 +168,8 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log('Invoice:', invoice);
+
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
